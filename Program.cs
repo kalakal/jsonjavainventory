4d82c6105663a66a0d7419f5a08376af1d6dae1a
kalakal <sayedfaizanqadri@gmail.com>
2022-11-06 22:43:11 +0530

    oops concept use case 1 added

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.Json;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace seriailzepr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string json = JsonSerializer.Serialize(new Product().GetProducts());
+            Console.WriteLine("json object \n");
+            Console.WriteLine(json);
+            Console.WriteLine("d-serialized json object");
+            var products = JsonSerializer.Deserialize < ObservableCollection<Product<>>(json);
+            products.Tolist().forEach(p => Console.WriteLine(p.Name));
+
+        }
+    }
+    public class Product
+    {
+        public string Name { get; set; }
+
+        public double PricePerKg { get; set; }
+
+        public int Weight { get; set; }
+
+        public ObservableCollection<Product> GetProducts()
+        {
+            return new ObservableCollection<Product>();
+            {
+                new Product() { Name = "Rice", PricePerKg = 36.99, Weight = 1 };
+                new Product() { Name = "Pulses", PricePerKg = 54.99, Weight = 2 };
+                new Product() { Name = "Wheat", PricePerKg = 32.99, Weight = 5 };
+            };
+        }
+    }
+}
